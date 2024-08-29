684f39bd3932dceb88da0c891f54a46081cdc497
fengtiecheng <17615007230@163.com>
2024-08-29 14:15:53 +0800

    style(批量保存注入器): 代码注释调整
    1.代码注释调整

--- src/main/java/com/ftc/mybatisplustest/mybatisplus/CustomSqlInjector.java
+++ src/main/java/com/ftc/mybatisplustest/mybatisplus/CustomSqlInjector.java
@@ -20,7 +20,7 @@ public class CustomSqlInjector extends DefaultSqlInjector {
         //1.获取方法集合
         List<AbstractMethod> methodList = super.getMethodList(mapperClass);
 
-        //2.新增批量查询方法设置,更新时自动填充的字段,不用插入值
+        //2.新增批量插入方法设置,更新时自动填充的字段,不用插入值
         methodList.add(new InsertBatchSomeColumn(i -> i.getFieldFill() != FieldFill.UPDATE));
 
         //3.返回
